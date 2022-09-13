a530cf90d9aafe478120d139c633a3f05e0de592
eswaranthangavel23 <84610856+eswaranthangavel23@users.noreply.github.com>
2022-09-13 13:02:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button area-aria-label="Click to submit">Submit</button>
+    </div>
+  );
+}
+
+export default App;
